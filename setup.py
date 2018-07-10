f5a32166e8144c66108f8d2611db804f3bcc11f9
Adrien Barbaresi <adrien.barbaresi@oeaw.ac.at>
2018-07-10 18:42:28 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -42,8 +42,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python',
-        #'Programming Language :: Python :: 2',
-        #'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
